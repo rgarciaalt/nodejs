e107c969adf01f2b5ba17f4e462b250bd195c663
Roberto García <rgarciaalt@gmail.com>
2023-06-06 16:35:47 +0200

    resolvedns

--- app.js
+++ app.js
@@ -3,11 +3,33 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// la configuración está en .env
+require('dotenv').config()
+
+const dns=require('node:dns')
+
+dns.resolveNs('segorbe.es',(err,addresses) => {
+    addresses.forEach((dir) => {
+       console.log(dir)
+    })
+})
+
+
+// conexión a base de datos
+const mongoose = require('mongoose')
+
+const dbname = "veterinaria"
+const uri = `mongodb+srv://${process.env.USUARIO}:${process.env.PASSWORD}@bd1.iqwo3h8.mongodb.net/${dbname}`
+
+mongoose.connect(uri)
+  .then( () => console.log('Base de datos conectada'))
+  .catch(e => console.log(e))
+
 // motor de plantillas
 
 app.set('view engine','ejs')
 app.set('views',__dirname +'/views')
-
+ 
  app.use(express.static(__dirname+'/public'))
 
  // rutas Web
--- router/Mascotas.js
+++ router/Mascotas.js
@@ -1,14 +1,26 @@
 const express= require('express')
 const router = express.Router()
 
-router.get('/',(req,res) => {
-    res.render("mascotas", {
-        arrayMascotas: [
-            {id:'1',nombre:'rex', descripcion:'descripcion'},
-            {id:'2',nombre:'chanchan', descripcion:'descripcionc'},
-            {id:'3',nombre:'perro', descripcion:'descripcionp'}
-        ]
+const Mascota = require('../models/mascota')
+
+router.get('/',async(req,res) => {
+   try {
+       const arrayMascotasDB = await Mascota.find()
+       console.log(arrayMascotasDB)
+       res.render("mascotas", { arrayMascotas : arrayMascotasDB
+        //arrayMascotas: [
+        //    {id:'1',nombre:'rex', descripcion:'descripcion'},
+        //    {id:'2',nombre:'chanchan', descripcion:'descripcionc'},
+        //    {id:'3',nombre:'perro', descripcion:'descripcionp'}
+        //]
     })
+   } catch {
+      console.log(error)
+   }
+
+
+
+    
 })
 
 
